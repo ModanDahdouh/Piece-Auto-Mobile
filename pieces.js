8625c961e0c7558e69e1e000d7bf6511216dc347
ModanDahdouh <mrdahdouhmodan@gmail.com>
2023-07-29 22:43:57 +0200

    localStorage

--- pieces.js
+++ pieces.js
@@ -1,19 +1,22 @@
-import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
-// Récuperation des pièces eventuellement stockées dans le localStorage
+import {
+    ajoutListenersAvis,
+    ajoutListenerEnvoyerAvis,
+    afficherAvis,
+} from "./avis.js";
+//Récupération des pièces eventuellement stockées dans le localStorage
 let pieces = window.localStorage.getItem("pieces");
-// Récupération des pièces depuis le fichier JSON
+
 if (pieces === null) {
+    // Récupération des pièces depuis l'API
     const reponse = await fetch("http://localhost:8081/pieces/");
     pieces = await reponse.json();
-
-    //Transformation des pièces en JSON
-    const valeurPieces = JSON.stringify("pieces", valeurPieces);
-    // Stokage des informations dans le localStorage
-    window.localStorage.setItem("pieces.valeurPieces");
+    // Transformation des pièces en JSON
+    const valeurPieces = JSON.stringify(pieces);
+    // Stockage des informations dans le localStorage
+    window.localStorage.setItem("pieces", valeurPieces);
 } else {
     pieces = JSON.parse(pieces);
 }
-
 // on appel la fonction pour ajouter le listener au formulaire
 ajoutListenerEnvoyerAvis();
 
@@ -24,6 +27,7 @@ function genererPieces(pieces) {
         const sectionFiches = document.querySelector(".fiches");
         // Création d’une balise dédiée à une pièce automobile
         const pieceElement = document.createElement("article");
+        pieceElement.dataset.id = pieces[i].id;
         // Création des balises
         const imageElement = document.createElement("img");
         imageElement.src = article.image;
@@ -62,6 +66,17 @@ function genererPieces(pieces) {
 }
 
 genererPieces(pieces);
+
+for (let i = 0; i < pieces.length; i++) {
+    const id = pieces[i].id;
+    const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+    const avis = JSON.parse(avisJSON);
+
+    if (avis !== null) {
+        const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+        afficherAvis(pieceElement, avis);
+    }
+}
 
 //gestion des bouttons
 const boutonTrier = document.querySelector(".btn-trier");
